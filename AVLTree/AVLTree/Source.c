d0b1ff7f9f13b2f0a07588f03099c3c516a04918
Levente Szénási <senasilevixd@gmail.com>
2022-03-22 19:32:58 +0100

    avl util

--- AVLTree/AVLTree/Source.c
+++ AVLTree/AVLTree/Source.c
@@ -5,125 +5,93 @@
 
 typedef struct node
 {
-	int value;
+	int key;
+	int height;
 	struct node* left;
 	struct node* right;
-	struct node* parent;
-	int leaf;
 } NODE;
 
-void insert(NODE* root);
-void insertNode(NODE* root, int value);
-void printNodes(NODE* root);
+NODE* insertNode(NODE* node, int key);
+NODE* newNode(int key);
+NODE* leftRotate(NODE* x);
+int getHeight(NODE* node);
+int getBalance(NODE* node);
 
-void main()
-{
-	//initialising base node
+void main() {
 	NODE* root = NULL;
-	root = (NODE*)malloc(sizeof(NODE));
-	root->value = 20;
-	root->left = root->right = NULL;
-	root->leaf = 1;
-	root->parent = NULL;
-	//adding base nodes
-	insertNode(root, 15);
-	insertNode(root, 17);
-	insertNode(root, 11);
-	insertNode(root, 21);
-	insertNode(root, 26);
-	insert(root);
-	insert(root);
-	printNodes(root);
 	return 0;
 }
 
-void insert(NODE* root)//user guided insert
-{
-	int value;
-	//fetching node value
-	printf("Enter node value: ");
-	scanf("%d", &value);
-	insertNode(root, value);
-	printf("Node successfully inserted.\n");
-	return 0;
-}
-
-void insertNode(NODE* root, int value)//actual insert based on acquired value
-{
-	NODE* akt = root;
-	NODE* parent = NULL;
-	//traversing tree
-	while (akt != NULL)
-	{
-		if (akt->value == value)
-		{
-			printf("Node already exists!\n");
-			return 0;
-		}
-		else if (akt->value < value)
-		{
-			parent = akt;
-			akt = akt->right;
-		}	
-		else
-		{
-			parent = akt;
-			akt = akt->left;
-		}
-	}
-	//inserting node
-	akt = (NODE*)malloc(sizeof(NODE));
-	akt->value = value;
-	akt->left = akt->right = NULL;
-	akt->leaf = 1;
-	akt->parent = parent;
-	akt->parent->leaf = 0;
-	if (akt->parent->value < value)
-		akt->parent->right = akt;
+// Return height for ptr
+int getHeight(NODE* node) {
+	if(node == NULL)
+		return 0;
 	else
-		akt->parent->left = akt;
-	return 0;
+		return node->height;
 }
 
-void printNodes(NODE* root)
-{
-	NODE* akt = root;
-	//printing algorithm
-	do
-	{
-		printf(" %d;", akt->value);
-		if (akt->leaf == 1)
-		{
-			if (akt == root)
-				return;
-			if (akt->value < akt->parent->value)//BT2
-			{
-				while (akt->value < akt->parent->value) {
-					akt = akt->parent;
-					if (akt == root)
-						return;
-				}
-			}
-			if (akt->value > akt->parent->value)//BT1
-			{
-				do {
-					akt = akt->parent;
-				} while (akt->left == NULL);
-				akt = akt->left;
-				continue;
-			}
-		}
-		if (akt->right != NULL)//branch right
-		{
-			akt = akt->right;
-			continue;
-		}
-		else//branch left
-		{
-			akt = akt->left;
-			continue;
-		}
-		
-	} while (akt != root);
+// Left rotate
+NODE* leftRotate(NODE* x) {
+	NODE* y = x->right;
+	NODE* temp = y->left;
+
+	y->left = x;
+	x->right = temp;
+
+	x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+	y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+
+	return y;
+}
+
+// Right rotate
+NODE* rightRotate(NODE* x) {
+	NODE* y = x->left;
+	NODE* temp = y->right;
+
+	y->right = x;
+	x->left = temp;
+
+	x->height = max(getHeight(x->left), getHeight(x->right)) + 1;
+	y->height = max(getHeight(y->left), getHeight(y->right)) + 1;
+
+	return y;
+}
+
+NODE* newNode(int key) {
+	NODE* node = (NODE*)malloc(sizeof(NODE));
+	node->height = 1;
+	node->key = key;
+	node->left = node->right = NULL;
+	return &node;
+}
+
+// Returns teh balance factor for the node
+int getBalance(NODE* node) {
+	if (node == NULL)
+		return 0;
+	else
+		return getHeight(node->left) - getHeight(node->right);
+}
+
+// Insert a new node
+NODE* insertNode(NODE* node, int key){
+	int balance;
+	// Recursive function to traverse tree and find place to insert.
+	if (node == NULL)
+		return newNode(key);
+	if (key > node->key)
+		node->right = insertNode(node->right, key);
+	else if (key < node->key)
+		node->left = newNode(node->left, key);
+	else
+		return node;
+
+	// Update the balance factor of visited nodes
+	node->height = max(node->left->height, node->right->height) + 1;
+
+	balance = getBalance(node);
+
+
 	return 0;
 }
